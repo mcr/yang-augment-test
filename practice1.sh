a44b4921596939f187eca2e9d0507a0f74163daf
Michael Richardson <mcr@sandelman.ca>
2022-11-13 21:04:39 +0100

    mark each module, and use proper prefixes

--- practice1.sh
+++ practice1.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+echo MODULE A
 pyang -f tree --tree-print-groupings module-A.yang
+
+echo
+echo MODULE B
 pyang -f tree --tree-print-groupings module-B.yang
+
+echo
+echo MODULE C
 pyang -f tree --tree-print-groupings module-C1.yang
+
+echo
+echo MODULE D
+pyang -f tree --tree-print-groupings module-D1.yang
 
