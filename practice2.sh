1c344d73eb5336791b7462143ed1de4d7764d748
Michael Richardson <mcr@sandelman.ca>
2022-11-13 21:28:58 +0100

    added D2 with wrong augment method

--- practice2.sh
+++ practice2.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+
+echo
+echo MODULE B
 pyang -f tree --tree-print-groupings module-B2.yang
+
+echo
+echo MODULE C
 pyang -f tree --tree-print-groupings module-C2.yang
+
+echo
+echo MODULE D
+pyang -f tree --tree-print-groupings module-D2.yang
 
