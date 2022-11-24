08c29616bde70a24cee21c49ff24359c71091b1b
Michael Richardson <mcr@sandelman.ca>
2022-11-23 19:44:33 -0500

    updated with SID allocation attempt

--- practice3.sh
+++ practice3.sh
@@ -8,16 +8,22 @@ echo
 echo MODULE A
 pyang ${INC} -f tree --tree-print-groupings module-A3.yang
 
+# some private use SID space at 60,000.
+pyang ${INC} --sid-list --sid-generate-file 62450:50 module-A3.yang
+
 echo
 echo MODULE B
 pyang ${INC} -f tree --tree-print-groupings module-B3.yang
+pyang ${INC} --sid-list --sid-update-file module-A3@unknown.sid module-B3.yang
 
 echo
 echo MODULE C
 pyang ${INC} -f tree --tree-print-groupings module-C3.yang
+pyang ${INC} --sid-list --sid-update-file module-A3@unknown.sid module-C3.yang
 
 echo
 echo MODULE D
-pyang ${INC} -f tree --tree-print-groupings module-D3.yang
+pyang ${INC} -f tree module-D3.yang
+pyang ${INC} --sid-list --sid-update-file module-A3@unknown.sid module-D3.yang
 exit 0
 
